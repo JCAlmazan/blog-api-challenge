d04c20faee725caf81b49f2c5e80bb4cbc698192
juancruz_94_ <juancruz_94_@hotmail.com>
2021-10-22 10:51:43 -0300

    post findById controller

--- controllers/post.js
+++ controllers/post.js
@@ -6,12 +6,29 @@ module.exports = {
   async list(req, res) {
     try {
       const posts = await Post.findAll({
-        order: [ ['createdAt', 'DESC'] ]
+        order: [['createdAt', 'DESC']]
       })
       res.status(201).send(posts)
     } catch (e) {
       console.log(e)
       res.status(500).send(e)
+    }
+  },
+
+  // Find a post by id
+  async findById(req, res) {
+    try {
+      const posts = await Post.findOne({
+        where: { name: req.params.id }
+      })
+      if (posts) {
+        res.status(201).send(posts)
+      } else {
+        res.status(404).send("Post Not Found")
+      }
+    } catch (e) {
+      console.log(e)
+      res.status(400).send(e)
     }
   },
 
